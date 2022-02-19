7fac731408cd12ebb0ae9833bd471a90b3f13f4c
SametCimen1 <cimensamet338@gmail.com>
2022-02-18 23:36:19 -0500

    added module

--- tutorial/mysite/mysite/settings.py
+++ tutorial/mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "main.apps.MainConfig",#we need to tell django that we need some more dependencies
 ]
 
 MIDDLEWARE = [
